21ab00f2eba048dee7fd6ccc9668aa74715e7981
bijesh-09 <bijeshkaranjit@gmail.com>
2026-07-28 10:15:43 +0000

    added some utils

--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_pwd(password: str):
     return pwd_context.hash(password)
+
+def verify_pwd(plain_pwd, hashed_pwd):
+    return pwd_context.verify(plain_pwd, hashed_pwd)
